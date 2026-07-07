8ff4b57f07a53de598eed4816bc9dd569c44910f
moza610 <agata.giarda@gmail.com>
2026-07-07 17:19:03 +0200

    porta

---
+++ Assets/Scripts/PuzzleDoorCode.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PuzzleDoorCode : MonoBehaviour
+{
+    public string CorrectCode = "124569";
+    private string currentInput = "";
+
+    public Event OnCodeCorrect;
+    public Event OnCodeWrong;
+
+    void AddDigit(string digit)
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
