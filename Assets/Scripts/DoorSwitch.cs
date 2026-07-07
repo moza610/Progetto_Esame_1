f763d8a116e051e53b753b06bd01fd44a17004c9
Elena <elena_scicolone@studenti.naba.it>
2026-07-07 17:44:46 +0200

    script porte (assegnati)

---
+++ Assets/Scripts/DoorSwitch.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour
+{
+    
+    private DoorBehaviour doorBehaviour;
+
+    private void Awake()
+    {
+        doorBehaviour = GetComponent<DoorBehaviour>();
+    }
+
+    /*private void OnMouseDown()
+    {
+        doorBehaviour.IsDoorOpen = !doorBehaviour.IsDoorOpen;
+    }*/
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            doorBehaviour.IsDoorOpen = !doorBehaviour.IsDoorOpen;
+        }
+        if (doorBehaviour.IsDoorOpen)
+        {
+            doorBehaviour.StartCoroutine(doorBehaviour.CloseDoorAfterDelay(5f));
+        }
+    }
+}
